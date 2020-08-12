fa42e640ca9a97d67c36dfe747628741fe782b46
ianwood103 <ianwood103@gmail.com>
2020-08-11 21:45:20 -0400

    Set up logic to load json data onto webpage

---
+++ main.js
@@ -0,0 +1,31 @@
+var base;
+var table;
+var players
+$(document).ready(function() {
+    $.getJSON("https://s3.amazonaws.com/scrapy.crawled.data/MLBPlayerPredictions/data.json", function(json) {
+        players = json;
+        fillGoodTable();
+        fillBadTalbe();
+    });
+});
+
+function fillGoodTable() {
+    table = document.getElementById('good-player-table').rows
+    for(var i = 0; i < table.length - 1; i++) {
+        fillBatter(i + 1, "good")
+    }
+}
+
+function fillBadTable() {
+    table = document.getElementById('bad-player-table').rows
+    for(var i = 0; i < table.length - 1; i++) {
+        fillBatter(i + 1, "bad")
+    }
+}
+
+function fillBatter(order, type) {
+    base = type + "-player-" + String(order);
+    $(base + ".player-headshot img").attr('src', players[order - 1]["headshot"]);
+    $(base + ".player").html(players[order - 1]["player"]);
+    $(base + ".player-logo img").attr("src", players[order - 1]["logo"]);
+}
