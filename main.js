d24fad8cd562f121ca69fdb7c89a73200b793720
ianwood103 <ianwood103@gmail.com>
2020-08-11 23:26:38 -0400

    edited json loading code

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 var base;
 var table;
-var players
+var players;
 $(document).ready(function() {
     $.getJSON("https://s3.amazonaws.com/scrapy.crawled.data/MLBPlayerPredictions/data.json", function(json) {
         players = json;
         fillGoodTable();
-        fillBadTalbe();
+        fillBadTable();
     });
 });
 
 function fillGoodTable() {
-    table = document.getElementById('good-player-table').rows
+    table = document.getElementById('good-player-table').rows;
     for(var i = 0; i < table.length - 1; i++) {
-        fillBatter(i + 1, "good")
+        fillBatter(i + 1, "good");
     }
 }
 
 function fillBadTable() {
     table = document.getElementById('bad-player-table').rows
     for(var i = 0; i < table.length - 1; i++) {
-        fillBatter(i + 1, "bad")
+        fillBatter(i + 6, "bad");
     }
 }
 
 function fillBatter(order, type) {
+    if (type == "bad") {
+        order -= 5;
+    }
     base = type + "-player-" + String(order);
-    $(base + ".player-headshot img").attr('src', players[order - 1]["headshot"]);
-    $(base + ".player").html(players[order - 1]["player"]);
-    $(base + ".player-logo img").attr("src", players[order - 1]["logo"]);
+    $("#" + base + " .player-headshot img").attr('src', players[order - 1]["headshot"]);
+    $("#" + base + " .player").html(players[order - 1]["player"]);
+    $("#" + base + " .player-logo img").attr("src", players[order - 1]["logo"]);
 }
