07f1be09e3f4c101cb96bbb32d7ea4fedb0cc094
ianwood103 <ianwood103@gmail.com>
2020-08-12 19:42:38 -0400

    Edited up json code to load all table items correctly.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var players;
 $(document).ready(function() {
     $.getJSON("https://s3.amazonaws.com/scrapy.crawled.data/MLBPlayerPredictions/data.json", function(json) {
         players = json;
+        console.log(players);
         fillGoodTable();
         fillBadTable();
     });
@@ -11,24 +12,27 @@ $(document).ready(function() {
 
 function fillGoodTable() {
     table = document.getElementById('good-player-table').rows;
-    for(var i = 0; i < table.length - 1; i++) {
+    for(var i = 0; i < 5; i++) {
         fillBatter(i + 1, "good");
     }
 }
 
 function fillBadTable() {
     table = document.getElementById('bad-player-table').rows
-    for(var i = 0; i < table.length - 1; i++) {
-        fillBatter(i + 6, "bad");
+    for(var i = 0; i < 5; i++) {
+        fillBatter(i + 1, "bad");
     }
 }
 
 function fillBatter(order, type) {
-    if (type == "bad") {
-        order -= 5;
-    }
     base = type + "-player-" + String(order);
+    if (type == "bad") {
+        order += 5;
+    }
     $("#" + base + " .player-headshot img").attr('src', players[order - 1]["headshot"]);
+    console.log(players[order - 1]["headshot"])
     $("#" + base + " .player").html(players[order - 1]["player"]);
+    console.log(players[order - 1]["player"])
     $("#" + base + " .player-logo img").attr("src", players[order - 1]["logo"]);
+    console.log(players[order - 1]["logo"])
 }
